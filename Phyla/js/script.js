528f95eb67344a95c6ebf424544de7e0eef73e1a
Kaustavha <kaustav_2007@hotmail.com>
2013-07-24 22:07:23 -0400

    Phyla responsive, hate for github now at 85%, why is this taking 3 commits and the  CLI??

---
+++ Phyla/js/script.js
@@ -0,0 +1,36 @@
+$("#intro").delay(2000).fadeOut(400);
+$("#intro").scroll(function(){
+	$("#intro").fadeOut('fast');
+});
+
+$("div.biosHolder").children().click(
+	function(){
+		var id = $(this).attr('id');
+		$("div.bio").fadeOut();
+		$("div.bio").addClass("hide");
+		$("#bio"+id).removeClass("hide");
+		$("#bio"+id).fadeIn();
+	});
+$("div.card").click(
+	function() {
+		var id = $(this).attr('id');
+		$('#artists').fadeOut();
+		$('#artistBios').removeClass("hide");
+		$('#artistBios').fadeIn();
+		$("#bio"+id).removeClass("hide");
+	});
+
+//The following function is responsible for the navbar functionality
+$("li a").click(
+	function(){		
+
+		var a=["#soundCloud","#artistBios","#artists","#bandCamp","#about","#music"];	    
+	    var data = $(this).data("pointer");
+	  
+	    for (i=0;i<a.length;i++){
+	    	$(a[i]).fadeOut("slow");
+	        $(a[i]).addClass("hide");
+	};	
+        $(data).removeClass("hide");
+        $(data).fadeIn("slow");
+	});
