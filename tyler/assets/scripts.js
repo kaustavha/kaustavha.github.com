51cad1a5c4d9e631f161d2d0bbad8991091d3d6a
kaustavha <kaustav_2007@hotmail.com>
2014-03-22 04:19:43 -0400

    Changes for tylers gate seventeen play website -> --Poster image --Effects now working --Header --Links to trailer & kickstarter, and fn to view poster --minor wording and arrangement changes

--- tyler/assets/scripts.js
+++ tyler/assets/scripts.js
@@ -1,5 +1,43 @@
-$(document).ready(function() {
-  $('.bg .before-dom-ready').removeClass('.before-dom-ready');
-  $('head h1').fadeIn();
-  return $('.synopsis').fadeIn();
+$(function() {
+  var dom, scrollAnim, ww;
+  dom = $('html, body');
+  ww = $(window).width();
+  scrollAnim = function() {
+    $(window).one("scroll", function() {
+      return setTimeout(function() {
+        $('.poster').fadeOut("fast");
+        $('.title').css("position", "fixed").fadeIn("medium");
+        dom.animate({
+          scrollTop: 0
+        }, 200);
+        return $('.title').fadeIn("fast", function() {
+          return $('#firstInfo').fadeIn("fast", function() {
+            return $('.synopsis').fadeIn("fast");
+          });
+        });
+      }, 300);
+    });
+  };
+  $('#poster').click(function(e) {
+    e.preventDefault();
+    $('.poster').fadeIn("fast");
+    dom.scrollTop(0);
+    setTimeout(function() {
+      return scrollAnim();
+    }, 200);
+  });
+  if (ww < 1479) {
+    $('.title').css("font-size", "180%");
+  }
+  if (ww < 900) {
+    $('.title').css("font-size", "120%");
+    $('.cast-holder').css("height", "840px");
+    $('.right').css("float", "center").css("margin", "0px");
+    $('.left').css("float", "center").css("margin", "0px");
+  }
+  $('.bg').removeClass("before-dom-ready");
+  dom.fadeIn("medium");
+  $.backstretch("./assets/boarding-gate.jpg");
+  $('.poster img').fadeIn("medium");
+  scrollAnim();
 });
