4c86202e06c346bd928662e1c28fea216b44b878
Kaustav Haldar <hi@kaustav.me>
2020-06-20 20:05:36 -0400

    Attempt to fix strapi asset routing issue

--- blog/tutorial/strapi_gatsby-tests/strapi-starter-next-blog/frontend/next.config.js
+++ blog/tutorial/strapi_gatsby-tests/strapi-starter-next-blog/frontend/next.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+const isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    assetPrefix: isDev ? '' : './', // fix assets on gh pages ; ref: https://github.com/vercel/next.js/issues/2581
   },
 }
---
+++ blog/tutorial/strapi_gatsby-tests/strapi-starter-next-blog/frontend/out/_next/static/BjGqUErNPpGoeRneA_CP4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/article/[id]":["static\u002Fchunks\u002F75fc9c18.3cfc8ccb85c1d7a11215.js",a],"/category/[id]":[a],"/index":[a]}}("static\u002Fchunks\u002F3c51f93c4a5944320e24af90b0d97554c9a6b8a8.5e14c7ad377f12bc4b6c.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
